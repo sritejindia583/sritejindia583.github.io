db1906b5e070d9ecce3f4dcaf5b884104a00c014
sritejindia583 <sritejindia@outlook.com>
2016-09-27 07:11:00 +0530

    Adding images and service functionality

---
+++ services/imageService.js
@@ -0,0 +1,18 @@
+/**
+ * Created by sritej583 on 27/9/16.
+ */
+
+'using strict'
+myApp.factory('imageService', function () {
+    var imagesData = [
+        {name: 'v.o.kuma', image: 'images/vijay kumar.jpg', description: 'A very nice guy'},
+        {name: 'soniya.khandhar', image: 'images/soniya.jpg', description: 'She is very sweet girl'},
+        {name: 'sanjeev.kumar.suman', image: 'images/sanjeev.jpg', description: 'A very good resource teamlead'},
+        {name: 'rishabh.d.arora', image: 'images/rishabh.jpg', description: 'A delhi wala'}
+    ]
+    return {
+        getImages: function () {
+            return imagesData
+        }
+    }
+})
